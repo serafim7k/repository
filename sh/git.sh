609d50f8bd4123070b3542b690c0942324e135a3
serafim789 <mycomputer10j12@gmail.com>
2022-10-07 09:24:28 +0300

    [A

--- sh/git.sh
+++ sh/git.sh
@@ -1,12 +1,5 @@
 #!/bin/bash
 
-read -p "Enter the remote: " remote
-read -p "Enter the commin name: " commit
-read -p "Enter the branch name: " branch
+read -p "Enter the git function name: " git
 
-git init
-git add .
-git commit -m "$commit"
-git remote remove origin
-git remote add origin $remote
-git push -u origin $branch
+sh sh/$git.sh
